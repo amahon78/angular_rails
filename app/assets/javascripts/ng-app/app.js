0a1541d127bd05163161b96987617ea33ffe900e
amahon78 <amahon78@gmail.com>
2014-10-29 20:43:18 +1000

    first commit

---
+++ app/assets/javascripts/ng-app/app.js
@@ -0,0 +1,29 @@
+/**
+ * Created with JetBrains RubyMine.
+ * User: alex
+ * Date: 28/10/14
+ * Time: 8:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+angular
+    .module('AngularRails', [
+    'ngRoute',
+    'templates'
+    ])
+    .config(function($routeProvider, $locationProvider){
+        $routeProvider
+            .when('/', {
+                templateUrl: 'home.html',
+                controller: 'HomeCtrl'
+
+            })
+            .when('/page',{
+                templateUrl: 'page.html',
+                controller: 'PageCtrl'
+            });
+
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+    });
---
+++ app/assets/javascripts/ng-app/controllers/page.js
@@ -0,0 +1,5 @@
+angular.module('AngularRails')
+    .controller('PageCtrl', function($scope){
+        $scope.things=['Another','array', 'of things', 'To see'];
+
+    });
